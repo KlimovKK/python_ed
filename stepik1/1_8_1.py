a8b0e7e1d459408be5446c7fb4f976d1f7c7462b
Konstantin Klimov <zmioolun@mail.ru>
2023-07-17 11:36:59 +0500

    init

---
+++ stepik1/1_8_1.py
@@ -0,0 +1,9 @@
+"""
+Тимофей обычно спит ночью XX часов
+и устраивает себе днем тихий час на YY минут.
+Определите, сколько всего минут Тимофей спит в сутки.
+"""
+
+sleep_night_hour = int(input())
+sleep_day_min = int(input())
+print(sleep_night_hour * 60 + sleep_day_min)
