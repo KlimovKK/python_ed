266528aaf0ee55b1dc899cd811a41f16849968cd
Konstantin K (home) <klimov.k66@mail.ru>
2023-08-20 12:38:38 +0500

    Part III exercise 4 Изменение логики Затем перепишите код для использования цикла for с конструкцией else, чтобы избавиться от явной логики индексации списка.

--- mark_lutz/p3_e4.py
+++ mark_lutz/p3_e4.py
@@ -1,11 +1,9 @@
 L = [1, 2, 4, 8, 16, 32, 64]
 X = 5
-i = 0
-while i < len(L):
-    if 2 ** X == L[i]:
-        print('at index', i)
+
+for i in L:
+    if 2 ** X == i:
+        print((2 ** X), 'was found at', L.index(i))
         break
-    else:
-        i = i + 1
 else:
     print(X, 'not found')
