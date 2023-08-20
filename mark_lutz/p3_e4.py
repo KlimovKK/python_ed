e5423d84619b03fee7880f91fc80e06f9c54b125
Konstantin K (home) <klimov.k66@mail.ru>
2023-08-20 12:52:41 +0500

    Part III exercise 4 Изменение логики Наконец примените цикл for и списковый метод append генерации списка степеней 2 (L) вместо жесткого кодирования спискового литерала.

--- mark_lutz/p3_e4.py
+++ mark_lutz/p3_e4.py
@@ -1,6 +1,9 @@
-L = [1, 2, 4, 8, 16, 32, 64]
+L = []
 X = 5
-
+Y = int(input())
+for i in range(Y):
+    L.append(2 ** i)
+print(L)
 if (2 ** X) in L:
     print((2 ** X), 'was found at', L.index(2 **X))
 else:
