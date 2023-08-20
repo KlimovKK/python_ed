e07076879521413c7bcaa5cd8c366e0e9b3798d0
Konstantin K (home) <klimov.k66@mail.ru>
2023-08-20 12:44:58 +0500

    Part III exercise 4 Изменение логики Далее полностью устраните цикл, переписав код с использованием простого выражения с операцией членства in.

--- mark_lutz/p3_e4.py
+++ mark_lutz/p3_e4.py
@@ -1,9 +1,7 @@
 L = [1, 2, 4, 8, 16, 32, 64]
 X = 5
 
-for i in L:
-    if 2 ** X == i:
-        print((2 ** X), 'was found at', L.index(i))
-        break
+if (2 ** X) in L:
+    print((2 ** X), 'was found at', L.index(2 **X))
 else:
     print(X, 'not found')
