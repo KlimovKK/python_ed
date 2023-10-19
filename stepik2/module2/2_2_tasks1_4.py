7e4d96807ed6742ada252338c859472e277c3e31
Konstantin K (home) <klimov.k66@mail.ru>
2023-10-19 20:32:12 +0500

    "Поколение Python": курс для начинающих Урок 2.2

---
+++ stepik2/module2/2_2_tasks1_4.py
@@ -0,0 +1,18 @@
+print('Здравствуй, мир!')
+
+print(4, 8, 15, 16, 23, 42)
+
+print(4)
+print(8)
+print(15)
+print(16)
+print(23)
+print(42)
+
+print('*')
+print('**')
+print('***')
+print('****')
+print('*****')
+print('******')
+print('*******')
---
+++ stepik2/module2/2_2_tasks5_8.py
@@ -0,0 +1,22 @@
+name = input()
+print('Привет,', name)
+
+
+team = input()
+print(team, '- чемпион!')
+
+
+str1 = input()
+str2 = input()
+str3 = input()
+print(str1)
+print(str2)
+print(str3)
+
+
+str1 = input()
+str2 = input()
+str3 = input()
+print(str3)
+print(str2)
+print(str1)
