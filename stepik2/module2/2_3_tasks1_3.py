fd19de9504631ec81bbfeb606d42deb4307515b7
K. Klimov (work) <klimov.k66@mail.ru>
2023-10-20 10:00:40 +0500

    "Поколение Python": курс для начинающих Урок 2.3

---
+++ stepik2/module2/2_3_tasks1_3.py
@@ -0,0 +1,10 @@
+print('I', 'like', 'Python', sep='***')
+
+
+separator = input()
+str1, str2, str3 = input(), input(), input()
+print(str1, str2, str3, sep=separator)
+
+
+name = input()
+print('Привет,', name, end='!')
