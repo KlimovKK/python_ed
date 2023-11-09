7208956acd4b86a770edaee5f1f7a4d5f3d2f56d
K. Klimov (work) <klimov.k66@mail.ru>
2023-11-09 17:30:32 +0500

    "Поколение Python": курс для начинающих Урок 7.3, задачи 6-7

---
+++ stepik2/module4/7_3_tasks6_11.py
@@ -0,0 +1,24 @@
+#  Без нулей
+"""QTY = 10
+
+product = 1
+for _ in range(QTY):
+    num = int(input())
+    if num != 0:
+        product *= num
+
+print(product)"""
+
+
+#  Сумма делителей
+"""n = int(input())
+
+total = 0
+for i in range(1, n + 1):
+    if n % i == 0:
+        total += i
+
+print(total)"""
+
+
+#  Знакочередующаяся сумма
