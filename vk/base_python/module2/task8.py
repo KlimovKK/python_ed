a95d53d2260749bb08be88803e7514aacc36700c
K. Klimov (work) <klimov.k66@mail.ru>
2023-11-17 11:45:30 +0500

    VK Education. Курс "Базовый Python". Модуль 2, задание 8

---
+++ vk/base_python/module2/task8.py
@@ -0,0 +1,15 @@
+def repeat_deco(n=1):
+    def my_decorator(func):
+        def wrapper(*args, **kwargs):
+            for _ in range(n):
+                result = func(*args, **kwargs)
+            return result
+        return wrapper
+    return my_decorator
+
+
+code = []
+while data := input():
+    code.append(data)
+code = "\n".join(code)
+exec(code)
