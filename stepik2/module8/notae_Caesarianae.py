052914d46f600d648a18ee8cbe4d64a3b9cc68da
K. Klimov (work) <klimov.k66@mail.ru>
2023-12-07 14:49:42 +0500

    "Поколение Python": курс для начинающих Урок 15.5 "Шифр Цезаря" (доделать)

---
+++ stepik2/module8/notae_Caesarianae.py
@@ -0,0 +1,48 @@
+"""
+Программа, способная шифровать и дешифровать текст в соответствии с алгоритмом Цезаря.
+Запрашивает у пользователя следующие данные:
+направление: шифрование или дешифрование;
+язык алфавита: русский или английский;
+шаг сдвига (со сдвигом вправо)
+"""
+
+
+def caesars_cipher(letter):
+    alpha = ['АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ', 'ABCDEFGHIJKLMNOPQRSTUVWXYZ']
+    new_index = (alpha[language].find(letter) + step) % len(alpha[language])
+    if letter.islower():
+        return alpha[language][new_index].lower()
+    else:
+        return alpha[language][new_index]
+
+
+direction = input('Введите 1 для шифрования или 2 для дешифрования \n=>')
+while direction not in ['1', '2']:
+    print('Что-то пошло не так!')
+    direction = input('Введите 1 для шифрования или 2 для дешифрования \n=>')
+
+language = input('Введите язык алфавита: 1 - русский, 2 - английский \n=>')
+while language not in ['1', '2']:
+    print('Что-то пошло не так!')
+    language = input('Введите язык алфавита: 1 - русский, 2 - английский \n=>')
+language = int(language) - 1
+
+step = input('Введите шаг сдвига вправо (целое число)\n=>')
+while not step.isdigit():
+    print('Что-то пошло не так!')
+    step = input('Введите шаг сдвига вправо (целое число)\n=>')
+if direction == '1':
+    step = int(step)
+else:
+    step = int('-' + step)
+
+inp_text = input('Введите текст\n=>')
+
+out_text = ''
+for char in inp_text:
+    if char.isalpha():
+        out_text += caesars_cipher(char)
+    else:
+        out_text += char
+
+print(out_text)
