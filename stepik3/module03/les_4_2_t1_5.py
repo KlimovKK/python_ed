27627f089d1fe498067aff050e485a03d53e5373
K. Klimov (work) <klimov.k66@mail.ru>
2023-12-07 11:15:27 +0500

    "Поколение Python": курс для продвинутых Урок 4.2, задачи 1-5

---
+++ stepik3/module03/les_4_2_t1_5.py
@@ -0,0 +1,47 @@
+#  1
+list1 = [10, 20, [300, 400, [5000, 6000], 500], 30, 40]
+
+list1[2][2].append(7000)
+
+print(list1)
+
+
+#  2
+list1 = ['a', 'b', ['c', ['d', 'e', ['f', 'g'], 'k'], 'l'], 'm', 'n']
+sub_list = ['h', 'i', 'j']
+list1[2][1][2].extend(sub_list)
+
+print(list1)
+
+
+#  3
+list1 = [[1, 7, 8], [9, 7, 102], [6, 106, 105], [100, 99, 98, 103], [1, 2, 3]]
+maximum = -1
+
+for el in list1:
+    maximum = max(max(el), maximum)
+
+print(maximum)
+
+
+#  4
+list1 = [[1, 7, 8], [9, 7, 102], [102, 106, 105], [100, 99, 98, 103], [1, 2, 3]]
+
+for el in list1:
+    el.reverse()
+
+print(list1)
+
+
+#  5
+list1 = [[1, 7, 8], [9, 7, 102], [102, 106, 105], [100, 99, 98, 103], [1, 2, 3]]
+total = 0
+counter = 0
+
+for el in list1:
+    total += sum(el)
+    counter += len(el)
+
+res = total / counter
+
+print(res)
