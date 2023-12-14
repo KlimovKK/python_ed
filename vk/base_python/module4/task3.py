8a6e907c93f8ad7b5bd03ae846531bb377971e47
K. Klimov (work) <klimov.k66@mail.ru>
2023-12-14 11:49:30 +0500

    VK Education. Курс "Базовый Python". Модуль 4, задание 3

---
+++ vk/base_python/module4/task3.py
@@ -0,0 +1,16 @@
+from collections import defaultdict
+from typing import List, Tuple
+
+
+def fill_specializations(specializations: List[Tuple[str, str]]):
+    specs_dict = defaultdict(list)
+    for spec, name in specializations:
+        specs_dict[spec].append(name)
+    return dict(specs_dict)
+
+
+code = []
+while data := input():
+    code.append(data)
+code = "\n".join(code)
+exec(code)
