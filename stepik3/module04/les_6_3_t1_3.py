844a96d1ab64d7ae1169263c837942405fd56d9b
K. Klimov (work) <klimov.k66@mail.ru>
2024-04-04 14:12:50 +0500

    "Поколение Python": курс для продвинутых Урок 6.3, задачи 2, 3

--- stepik3/module04/les_6_3_t1_3.py
+++ stepik3/module04/les_6_3_t1_3.py
@@ -4,3 +4,26 @@ a, b, c = [int(input()) for _ in range(3)]
 coord = (-b / (2 * a), (4 * a * c - b ** 2) / (4 * a))
 
 print(coord)
+
+
+#  Конкурсный отбор
+def print_list(schoolkids, min_grade):
+    for schoolkid in schoolkids:
+        if schoolkid[1] >= min_grade:
+            print(*schoolkid)
+
+
+n = int(input())
+class_list = [tuple(int(i) if i.isdigit() else i for i in input().split()) for _ in range(n)]
+
+print_list(class_list, 1)
+print()
+print_list(class_list, 4)
+
+
+#  Последовательность Трибоначчи
+n = int(input())
+t1, t2, t3 = 1, 1, 1
+for i in range(n):
+    print(t1, end=' ')
+    t1, t2, t3 = t2, t3, t1 + t2 + t3
